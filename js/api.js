d09215cc71fe5127bd25f51614a3c78395da37b3
putijabdb1-create <putijabdb1@gmail.com>
2026-07-30 11:45:52 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,57 @@
-api.js
+/*************************************************
+ * PT PUTRA TIMUR JAYA
+ * Monitoring Teknisi
+ * API ENGINE V2
+ *************************************************/
+
+// ===============================
+// GANTI DENGAN URL WEB APP ANDA
+// ===============================
+
+const API_URL =
+"https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXXXXXX/exec";
+
+
+// ===============================
+// DATA GLOBAL
+// ===============================
+
+let monitoringData = [];
+
+
+// ===============================
+// LOAD DATA
+// ===============================
+
+async function loadMonitoring() {
+
+    try {
+
+        document.getElementById("detailWO").innerHTML =
+        "Mengambil data dari Google Sheet...";
+
+        const response = await fetch(
+            API_URL + "?action=getData"
+        );
+
+        monitoringData = await response.json();
+
+        console.log(monitoringData);
+
+        dashboardSummary();
+
+        buildWOList();
+
+        drawMarkers();
+
+    }
+
+    catch (err) {
+
+        console.error(err);
+
+        alert("Gagal mengambil data dari Apps Script");
+
+    }
+
+}
