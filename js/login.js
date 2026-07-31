c45b4d7b25191638762ec0924d220d6840d01522
putijabdb1-create <putijabdb1@gmail.com>
2026-07-31 09:18:37 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,17 +2,25 @@
  * LOGIN ENGINE
  *************************************************/
 
-document.getElementById("btnLogin").onclick = function(){
+document.getElementById("btnLogin").onclick=function(){
 
-    // sementara login tanpa API
-    // nanti diganti validasi Google Sheet
+    const username=document.getElementById("username").value;
+    const password=document.getElementById("password").value;
 
-    sessionStorage.setItem("login","true");
+    if(username=="admin" && password=="12345"){
 
-    sessionStorage.setItem("loginTime",Date.now());
+        sessionStorage.setItem("login","true");
+        sessionStorage.setItem("username",username);
+        sessionStorage.setItem("role","ADMIN");
+        sessionStorage.setItem("loginTime",Date.now());
+        sessionStorage.setItem("lastActivity",Date.now());
 
-    sessionStorage.setItem("lastActivity",Date.now());
+        location.href="index.html";
 
-    location.href="index.html";
+    }else{
 
-};
+        alert("Username atau Password salah.");
+
+    }
+
+}
