272c319211e5f750fd681dfad2844638e89dd588
putijabdb1-create <putijabdb1@gmail.com>
2026-07-31 09:16:19 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 /*************************************************
+ * SESSION CHECK
+ *************************************************/
+
+if(sessionStorage.getItem("login")!="true"){
+
+    location.replace("login.html");
+
+}
+
+/*************************************************
  * APP ENGINE
  *************************************************/
 
@@ -12,3 +22,60 @@ window.onload = function(){
         .getElementById("btnRefresh")
         .addEventListener("click", refreshMonitoring);
 };
+/*************************************************
+ * UPDATE LAST ACTIVITY
+ *************************************************/
+
+function updateActivity(){
+
+    sessionStorage.setItem(
+
+        "lastActivity",
+
+        Date.now()
+
+    );
+
+}
+
+document.addEventListener("click",updateActivity);
+
+document.addEventListener("keydown",updateActivity);
+
+document.addEventListener("mousemove",updateActivity);
+
+document.addEventListener("scroll",updateActivity);
+
+document.addEventListener("touchstart",updateActivity);
+
+/*************************************************
+ * AUTO LOGOUT 1 JAM
+ *************************************************/
+
+setInterval(function(){
+
+    const last = Number(
+
+        sessionStorage.getItem("lastActivity")
+
+    );
+
+    if(!last) return;
+
+    const idle = Date.now()-last;
+
+    if(idle > 60*60*1000){
+
+        alert(
+
+            "Session berakhir karena tidak ada aktivitas selama 1 jam."
+
+        );
+
+        sessionStorage.clear();
+
+        location.replace("login.html");
+
+    }
+
+},60000);
